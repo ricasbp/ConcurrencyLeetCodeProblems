b97ed09338d2a11557fcc90e1db6118066fde9e0
ricasbp <ricardoBaltazarPedro@hotmail.com>
2024-11-20 16:22:30 +0000

    Fix: Added documentation

--- src/main/java/org/example/Default.java
+++ src/main/java/org/example/Default.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Default {
+
+    // Default test to learn how to create Junit Tests:  https://www.youtube.com/watch?v=Zi67qUHbq-c&ab_channel=Randomcode
+
     public String getName(int number){
         if (number < 0) {
             return "a";
